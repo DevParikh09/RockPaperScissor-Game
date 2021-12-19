aa4980230b9662de1939bcd81debc711b2f412e0
devparikh9 <96355061+devparikh9@users.noreply.github.com>
2021-12-19 12:57:42 +0530

    Add files via upload

---
+++ RockPaperScissor2/app/src/main/java/com/example/rockpaperscissor/FirstFragment.java
@@ -0,0 +1,50 @@
+package com.example.rockpaperscissor;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.rockpaperscissor.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer gameStartSound = MediaPlayer.create(getActivity(), R.raw.game_start_sound_new);
+                gameStartSound.start();
+                NavHostFragment.findNavController(FirstFragment.this)   //Navigate to second fragment
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
